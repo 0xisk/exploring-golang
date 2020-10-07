59e1f9301f4f7da1c02f71bd43eefa67dc64afb3
iskanderandrew <iskander.s.andrews@gmail.com>
2020-10-07 13:10:04 +0200

    Creating a script for Variables

---
+++ freeCodeCamp/variables.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+// Variables Method 3
+var i int = 42
+
+func main()  {
+
+	// Variables Method 1
+	// var i int
+	// i = 42
+
+	// Variables Method 2
+	// var x int = 50
+	// x := 50
+
+	// fmt.Println(i + x)
+	fmt.Printf("%v, %T", i, i)
+}
